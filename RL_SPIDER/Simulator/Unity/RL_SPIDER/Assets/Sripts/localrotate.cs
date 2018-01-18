468a5a81ef83d03405b54060a7ad700193537fc6
saurabhjadhav1911 <saurabhjadhav1911@gmail.com>
2018-01-18 16:35:05 +0530

    hinge joint rotate

---
+++ RL_SPIDER/Simulator/Unity/RL_SPIDER/Assets/Sripts/localrotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class localrotate : MonoBehaviour {
+
+	// Use this for initialization
+	void start () {
+		 transform.RotateAround(Vector3.zero, Vector3.up,0);
+	}
+	// Update is called once per frame
+	void Update () {
+		 transform.RotateAround(Vector3.zero, Vector3.up,);
+	}
+}
