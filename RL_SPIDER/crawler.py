bed6909d6c90f513d3a40ed77602d33384635fe6
saurabhjadhav1911 <saurabhjadhav1911@gmail.com>
2018-02-23 10:13:52 +0530

    crawler math model start

---
+++ RL_SPIDER/crawler.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+class Crawler():
+	"""docstring for Crawler"""
+	def __init__(self, ):
+		__init__(self,l1=None,l2=None,b=None,h=None)
+
+		self.b=b or 4
+		self.h=h or 0.5
+		self.l1=l1 or 5
+		self.l2=l2 or 5
+
+	def render(self):
+		pass
+
+	def step(self,action):
+		
+
+		
